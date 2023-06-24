abc3edc8b896230d2796b9cff9c7d9cf2237ed4d
Qudoos <quddoos18@gmail.com>
2023-06-24 18:28:17 +0500

    posting data in http module

--- server.js
+++ server.js
@@ -12,57 +12,32 @@ const toDo = [
     }
 ]
 
+
 const server = http.createServer((req, res) => {
-    // seting status code for different methods
 
-    // we are making a response object to explain the status
 
-    // if the status is 4.xx then there is cliet side error and data will be null
-    const response = {
-        success: false,
-        data: null,
-        error: 'please add valid data'
-    }
-    // for showing text  on browser
-    // res.setHeader('Content-Type', 'text/plain')   
-
-    // for showing html on browser
-    // res.setHeader('Content-Type', 'text/html')
-    // res.setHeader('X-Powered-By', 'Node.js')
-
-    // for showing json on browser
-    // res.setHeader('Content-Type', 'application/json')
-    // res.setHeader('X-Powered-By', 'Node.js')
-
-    // we using res.writeHead to set header and status 
-
-    res.writeHead(404, {
+    res.writeHead(200, {
         'Content-Type': 'application/json',
         'X-Powered-By': 'Node.js'
     })
 
-    // res.setHeader(200, {
-    //     'X-Powered-By': 'Node.js',
-    //     'Content-Type': 'text/html'
-    // })
+    // we are using res.on method
+    let body = [];
 
-    // destructring
-    // const { url, method, header } = req
-    // res.write('<h1>hello</h1>')
-    // sending data in res.end() method instead of res.write
+    req.on('data', chunk => {
+        body.push(chunk)
+    }).on('end', () => {
+        body = Buffer.concat(body).toString()
+        console.log(body)
+    })
 
-    // when we not using response object
-    //     res.end(
-    //         JSON.stringify({
-    //             success: true,
-    //             data: toDo
-    //         })
-    //     )
-
-    // when we using response object
     res.end(
-        JSON.stringify(response)
+        JSON.stringify({
+            success: true,
+            data: toDo,
+        })
     )
+
 })
 
 
