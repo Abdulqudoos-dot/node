afc296b7f2be79250189a16d5dfafee9a1cfa07b
Qudoos <quddoos18@gmail.com>
2023-06-25 17:03:48 +0500

    using middleware of logger to log infomation of rout methods

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const port = process.env.PORT || 5000;
+// initializing middleware for log on information of our methods
 
-
-
+const logger = (req, res, next) => {
+    // console.log(req)
+    const { method, hostname, path, port } = req
+    console.log(`${method} ${hostname}${path} ${port}`)
+    next()
+}
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+app.use(logger)
 // using middleware for bootcap router
 
 app.use('/api/v1/bootcamps', require('./routs/bootcamps'))
-
+app.get('/', (req, res) => {
+    res.json({ success: true, msg: 'this is main page' })
+})
 // making express methods
 // fech all bootcamps
 // making a good api for mantainess
