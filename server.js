134ac5b22de90c442bfd5857dd67f22871e2a8eb
Qudoos <quddoos18@gmail.com>
2023-06-25 15:24:53 +0500

    using get post method in http

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { error } = require('console')
+
 const http = require('http')
 const PORT = 80
 const toDo = [
@@ -14,29 +14,50 @@ const toDo = [
 
 
 const server = http.createServer((req, res) => {
-
-
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'X-Powered-By': 'Node.js'
-    })
-
-    // we are using res.on method
-    let body = [];
-
-    req.on('data', chunk => {
+    const { method, url } = req
+    let body = []
+    req.on('data', (chunk) => {
         body.push(chunk)
     }).on('end', () => {
         body = Buffer.concat(body).toString()
-        console.log(body)
-    })
+        let status = 404
+        let response = {
+            success: false,
+            data: null,
+            error: 'please add correct credentials'
 
-    res.end(
-        JSON.stringify({
-            success: true,
-            data: toDo,
+        }
+        if (method === 'GET' && url === '/todo') {
+            status = 200
+            response.success = true
+            response.data = toDo
+            response.error = null
+
+        } else if (method === 'POST' && url === '/todo') {
+            const { id, name } = JSON.parse(body)
+            if (!id || !name) {
+                status = 404
+                response.success = false
+                response.data = null
+                response.error = 'please eneter id or name'
+            } else {
+
+                toDo.push({ id, name })
+                response.success = true
+                status = 201
+                response.data = toDo
+                response.error = null
+            }
+        }
+
+        res.writeHead(status, {
+            'Content-Type': 'application/json',
+            'X-Powered-By': 'Node.js'
         })
-    )
+
+
+        res.end(JSON.stringify(response))
+    })
 
 })
 
